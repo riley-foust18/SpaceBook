363564b49b1c7133d5926b7295f3e923eb3ebb3c
DoodleBob <riley.foust18@gmail.com>
2021-12-26 14:12:37 -0500

    created post and delete reaction routes

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -76,7 +76,41 @@ const thoughtController = {
         res.json(dbUserData);
       })
       .catch(err => res.json(err));
-  }
+  },
+
+  addReaction({params, body}, res) {
+    Thought.findOneAndUpdate(
+      {_id: params.thoughtId},
+      {$push: {reactions: body}},
+      {new: true}
+    )
+      .select("-__v")
+      .then(dbThoughtData => {
+        if (!dbThoughtData) {
+          res.status(404).json({ message: 'No thought found with this id!' });
+          return;
+        }
+        res.json(dbThoughtData);
+      })
+      .catch(err => res.status(400).json(err));
+  },
+
+  deleteReaction({params, body}, res) {
+    Thought.findOneAndUpdate(
+      {_id: params.thoughtId},
+      {$pull: {reactions: {reactionId: body.reactionId}}},
+      {new: true}
+    )
+      .select("-__v")
+      .then(dbThoughtData => {
+        if (!dbThoughtData) {
+          res.status(404).json({ message: 'No reaction found with this id!' });
+          return;
+        }
+        res.json(dbThoughtData);
+      })
+      .catch(err => res.status(400).json(err));
+  },
 }
 
 module.exports = thoughtController
--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -20,9 +20,9 @@ router
   .put(updateThought)
   .delete(deleteThought);
 
-// router
-//   .route('/:thoughtId/reactions')
-//   .post(addReaction)
-//   .delete(deleteReaction)
+router
+  .route('/:thoughtId/reactions')
+  .post(addReaction)
+  .delete(deleteReaction)
 
 module.exports = router;
